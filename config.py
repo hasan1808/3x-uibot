2a2adba2a5590630a3ac47ee20945fa14e4ae46c
hasan1808 <hasan1808@users.noreply.github.com>
2026-07-03 10:05:24 -0700

    Fix config: handle empty ADMIN_TELEGRAM_ID

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
-ADMIN_TELEGRAM_ID = int(os.getenv("ADMIN_TELEGRAM_ID", "0"))
+try:
+    ADMIN_TELEGRAM_ID = int(os.getenv("ADMIN_TELEGRAM_ID", "0"))
+except:
+    ADMIN_TELEGRAM_ID = 0
